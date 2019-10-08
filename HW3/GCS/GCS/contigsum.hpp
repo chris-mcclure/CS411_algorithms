472546f391118f01047920c190af1ad0b0accc4d
Chris McClure <chrismcclure2010@gmail.com>
2019-10-08 11:49:50 -0800

    initial commit of hw3 with gitignore

---
+++ HW3/GCS/GCS/contigsum.hpp
@@ -0,0 +1,13 @@
+//
+//  contigsum.hpp
+//  GCS
+//
+//  Created by Chris McClure on 10/7/19.
+//  Copyright © 2019 Chris McClure. All rights reserved.
+//
+
+#ifndef contigsum_h
+#define contigsum_h
+
+
+#endif /* contigsum_h */
---
+++ HW3/Inversions/Inversions/inversions.hpp
@@ -0,0 +1,13 @@
+//
+//  inversions.hpp
+//  Inversions
+//
+//  Created by Chris McClure on 10/7/19.
+//  Copyright © 2019 Chris McClure. All rights reserved.
+//
+
+#ifndef inversions_h
+#define inversions_h
+
+
+#endif /* inversions_h */
