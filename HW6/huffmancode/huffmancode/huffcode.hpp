c367568a27a7b70346f73323d99e71f749547c7e
Chris McClure <chrismcclure2010@gmail.com>
2019-12-07 16:07:15 -0900

    finished hw6

--- HW6/huffmancode/huffmancode/huffcode.hpp
+++ HW6/huffmancode/huffmancode/huffcode.hpp
@@ -1,3 +1,6 @@
+// Chris McClure
+// 12/6/19
+//
 // huffcode.hpp  UNFINISHED
 // Glenn G. Chappell
 // 29 Nov 2015
@@ -14,7 +17,24 @@
 #define FILE_HUFFCODE_H_INCLUDED
 
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <unordered_map>
+#include <memory>
+#include <queue>
+#include <string>
+
+/* Node struct for binary tree construction */
+struct Node{
+    Node(){}
+    Node(char key, int weight): _key(key), _weight(weight){}
+    Node(std::shared_ptr<Node> & leftNode, std::shared_ptr<Node> & rightNode, int weight):
+    _leftNode(leftNode), _rightNode(rightNode), _weight(weight){}
+    
+    std::shared_ptr<Node> _leftNode, _rightNode = nullptr;
+    int _weight = 0;
+    char _key = 0;
+};
 
 // Class HuffCode
 // Encoding & decoding using a Huffman code
@@ -27,18 +47,16 @@ public:
 
 // ***** HuffCode: general public functions *****
 public:
-
+    bool cmp(std::shared_ptr<Node> & lhs, std::shared_ptr<Node> & rhs);
     void setWeights(const std::unordered_map<char, int> & theweights);
-
     std::string encode(const std::string & text) const;
-    
     std::string decode(const std::string & codestr) const;
-    
+    void weightMap(Node & node, std::string code);
     std::string recurseDecode(std::string & codestr, std::string::iterator & first, std::string::iterator & last) const;
-    int weights = 0;
-// ***** HuffCode: data members *****
+    // ***** HuffCode: data members *****
 private:
-
+    Node root;
+    std::unordered_map<char, std::string> lookup_map;
 };  // End class HuffCode
 
 
